630cdeeef6e20c6bb2c566a762dea1abfc8e29f4
Bárbara Hedwig von Kostrisch <hedwigvonkostrisch@hotmail.com>
2024-02-05 18:55:35 -0300

    Understanding canva rectangles

--- script.js
+++ script.js
@@ -5,22 +5,42 @@ let ctx2 = screen.getContext("2d");
 let ctx3 = screen.getContext("2d");
 
 
-ctx.lineWidth = 3;
-ctx.strokeStyle = "#FF98AE";
-ctx.moveTo(0,0);
-ctx.lineTo(700,0);
-ctx.lineTo(350,200);
-ctx.stroke();
+
+// Rectangle border only
+// ctx.strokeStyle = "white";
+// ctx.strokeRect(20, 20, 100, 200)
 
 
-ctx2.moveTo(0,0);
-ctx2.lineTo(350,200);
-ctx2.stroke();
+// Purple Rectangle 
+// ctx.fillStyle = "purple";
+// ctx.fillRect(20, 20, 100, 200)
 
 
-ctx3.lineWidth = 10;
-ctx3.moveTo(0,0);
-ctx3.lineTo(0,500);
-ctx3.lineTo(700,500);
-ctx3.lineTo(700,0);
-ctx3.stroke();
+//Rectangle with border and cut part
+// ctx.rect(20, 20, 100, 200)
+// ctx.strokeStyle = "white";
+// ctx.fillStyle = "purple";
+// ctx.fill();
+// ctx.stroke();
+// ctx.lineWidth = 3;
+// ctx.clearRect(10, 10, 20, 20);
+
+
+// Carta desenhada
+// ctx.lineWidth = 3;
+// ctx.strokeStyle = "#FF98AE";
+// ctx.moveTo(0,0);
+// ctx.lineTo(700,0);
+// ctx.lineTo(350,200);
+// ctx.stroke();
+
+// ctx2.moveTo(0,0);
+// ctx2.lineTo(350,200);
+// ctx2.stroke();
+
+// ctx3.lineWidth = 10;
+// ctx3.moveTo(0,0);
+// ctx3.lineTo(0,500);
+// ctx3.lineTo(700,500);
+// ctx3.lineTo(700,0);
+// ctx3.stroke();
